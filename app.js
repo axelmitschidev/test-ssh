cc821465ea17bd96bfbe740145501b08a7534099
Axel Mitschi <axel.mitschi@gmail.com>
2022-02-02 14:43:27 +0100

    update test

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 const PORT = 8080
 
 app.get('/', (req, res) => {
-	res.send('test')
+	res.render('home.html.twig');
 })
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
 	console.log('Listen on http://localhost:' + PORT)
 })
